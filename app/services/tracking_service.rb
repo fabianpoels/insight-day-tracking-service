33f509987d0b7c82f35bbbcddf4a30aecdc59d04
Fabian Poels <fabian@fabianpoels.com>
2024-11-12 13:19:38 +0100

    extract language from tracking

--- app/services/tracking_service.rb
+++ app/services/tracking_service.rb
@@ -10,10 +10,18 @@ class TrackingService < ApplicationService
   end
 
   def call
+    # OS and device
     client = DeviceDetector.new(@user_agent)
     device = client.device_name
     os = "#{client.os_name} #{client.os_full_version}"
 
-    Tracking::TrackJob.perform_async(@datetime.to_s, @ip, os, device, @url, @referrer, @language, @uuid)
+    # extract languag from the headers
+    langs = @language.to_s.split(",").map do |lang|
+      l, q = lang.split(";q=")
+      [l, (q || '1').to_f]
+    end
+    language = langs.sort_by(&:last).map(&:first).last
+
+    Tracking::TrackJob.perform_async(@datetime.to_s, @ip, os, device, @url, @referrer, language, @uuid)
   end
 end
