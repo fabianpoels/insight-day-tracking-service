d64c3cba41a347a5b12d6bdd6dc6e379a6c89a34
Fabian Poels <fabian@fabianpoels.com>
2024-11-12 15:20:27 +0100

    dashboard update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
     @total_count = TrackingRequest.all.count
     @unique_visitors = TrackingRequest.select(:uuid).distinct.count
+    @requests_by_date = TrackingRequest.group('tracked_at::date').order('tracked_at::date').count
     @urls = TrackingRequest.group(:url).order(count: :desc).count
-    puts @urls.inspect
   end
 end
